52f9372954d4994773bdcdbf3fffa3c6d8c01d2b
JAKKA GOPALA KRISHNA <jakkakrishna2003@gmail.com>
2025-12-12 15:37:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Transpose of Matrix/transpose-of-matrix.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    vector<vector<int>> transpose(vector<vector<int>>& mat) {
+        // code here
+        int n = mat.size();
+        for (uint i = 0; i < n; ++i) {
+            for (uint j = i; j < n; ++j)
+                swap(mat[i][j], mat[j][i]);
+        }
+        return mat;
+    }
+};
+//GFG POTD solution for 12 December
