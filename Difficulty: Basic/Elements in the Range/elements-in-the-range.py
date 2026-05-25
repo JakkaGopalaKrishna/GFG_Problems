0c4bdf1ed1da37b4ba5e74834f9d951d5d6caca3
JAKKA GOPALA KRISHNA <jakkakrishna2003@gmail.com>
2026-05-25 09:11:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Elements in the Range/elements-in-the-range.py
@@ -0,0 +1,7 @@
+class Solution:
+    def checkElements(self, start, end, arr):
+        # code here
+        for i in range(start,end+1):
+            if i not in arr:
+                return False
+        return True 
